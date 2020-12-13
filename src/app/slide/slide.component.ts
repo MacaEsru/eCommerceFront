cc27db768ddddd596df9875af45a00cf147c8909
MacaEsru <163170@ids.upchiapas.edu.mx>
2020-12-12 22:01:59 -0600

    add sliderlyrics + navbarlyrics

--- src/app/slide/slide.component.ts
+++ src/app/slide/slide.component.ts
@@ -10,16 +10,24 @@ export class SlideComponent implements OnInit {
 
   images:any[]=[
     {
-      img: 'assets/images/imga.jpg'
+      img: 'assets/images/imga.jpg',
+      name: 'Creaciones Magik',
+      description:'Contamos con manualidades enfocados en las temáticas de tu agrado.'
     },
     {
-      img: 'assets/images/imgb.jpg'
+      img: 'assets/images/imgb.jpg',
+      name: 'Creaciones Magik',
+      description: 'Detalles para esa personita especial.'
     },
     {
-      img: 'assets/images/imgc.png'
+      img: 'assets/images/imgc.png',
+      name: 'Creaciones Magik',
+      description: 'Creaciones para algún tipo de evento o fiesta sin importar el enfoque.'
     },
     {
-      img: 'assets/images/imge.jpg'
+      img: 'assets/images/imge.jpg',
+      name:'Creaciones Magik',
+      description: 'Nuestras manualidades son definidas por ser únicas e inigualables.'
     },
   ];
 
